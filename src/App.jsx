20d924a2c191e4206e05ab8861204f5b40433d3c
AgustinFerres <agustinferres4403@gmail.com>
2022-12-05 13:06:06 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
+import Contact from "./Routes/Contact";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+            <Route path="/favs" element={<Favs/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/dentist/:id" element={<Detail/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
