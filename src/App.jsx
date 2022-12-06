8ec688ce267025876f4954a514e466928d9433e1
Gonzalo alvarez <alvarezgonzalo0022@gmail.com>
2022-12-06 09:19:34 -0300

    Uploading the Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home";
 import Favs from "./Routes/Favs";
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Card = ({ name, username, id }) => {
 
+
   const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
+    localStorage.setItem(`${username}`, JSON.stringify(id));
   }
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
+        <img src="#" alt="Odontologo" />
+        <Link to={`/dentist/${id}`}>{name}</Link>
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
